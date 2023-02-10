26af78820dd73afbf9dbd38573a62a372b47d9b2
Jorge <jgallegosoto12@gmail.com>
2023-02-10 10:28:13 +0100

    Creada y adaptada la interfaz

--- src/main/java/com/example/turtletorrent/HelloApplication.java
+++ src/main/java/com/example/turtletorrent/HelloApplication.java
@@ -8,15 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage g_stage;
     @Override
     public void start(Stage stage) throws IOException {
+        g_stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TurtleCloud Torrent");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/example/turtletorrent/HelloController.java
+++ src/main/java/com/example/turtletorrent/HelloController.java
@@ -1,14 +1,52 @@
 package com.example.turtletorrent;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button btnCargarTorrent;
+
+    @FXML
+    private ImageView imgSelected;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Ya se como funciona");
+    }
+
+
+
+    @FXML
+    protected void onAddTorrentClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar Imagen");
+
+        // Agregar filtros para facilitar la busqueda
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+
+        // Obtener la imagen seleccionada
+        File file = fileChooser.showOpenDialog(HelloApplication.g_stage);
+        welcomeText.setText(file.getName());
+    }
+    @FXML
+    protected void onCloseButton() {
+        System.exit(0);
     }
 }
