4a361978f488e97051f3e8f38bc06ebffc2f1272
Alfredo Lajara <alfredo_lajara@outlook.es>
2023-02-16 17:31:16 +0100

    Pal PC a arreglar esto

--- src/main/java/com/example/turtletorrent/HelloController.java
+++ src/main/java/com/example/turtletorrent/HelloController.java
@@ -1,18 +1,23 @@
 package com.example.turtletorrent;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
 
 public class HelloController {
+    public TextField txtIp;
+    public TextField txtPort;
+    public TableView<ObservableList<String>> tblArchivos;
+    public TableColumn clmNombre;
+    public TableColumn clmTamano;
     @FXML
     private Label welcomeText;
 
@@ -27,6 +32,13 @@ public class HelloController {
         welcomeText.setText("Ya se como funciona");
     }
 
+    @FXML
+    private void initialize() {
+    }
+
+    private Socket socket = null;
+    private OutputStream out = null;
+    private InputStream in = null;
 
 
     @FXML
@@ -48,5 +60,52 @@ public class HelloController {
     @FXML
     protected void onCloseButton() {
         System.exit(0);
+    }
+
+    public void onConnectClick(MouseEvent mouseEvent) throws IOException {
+        try {
+            socket = new Socket(txtIp.getText(), Integer.parseInt(txtPort.getText()));
+            if (socket.isConnected()) {
+                System.out.println("Connected");
+            }
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+            //takes input from socket
+            in = new DataInputStream(socket.getInputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+
+        byte[] recibidos = new byte[1024];
+
+        DatagramSocket socket = new DatagramSocket(Integer.parseInt((txtPort.getText())));
+
+        System.out.println("Esperando Datagrama'......... ");
+
+        DatagramPacket paqRecibido = new DatagramPacket(recibidos, recibidos.length);
+        socket.receive(paqRecibido); //recibo el datagrama
+        System.out.println(Arrays.toString(paqRecibido.getData()));
+        System.out.println(paqRecibido.getLength());
+        //convierto el datagrama a String obmitiendo los bytes vacios
+        String mensaje = new String(paqRecibido.getData(), 0, paqRecibido.getLength());
+        System.out.println(mensaje);
+        // put the mensaje content to a String[][]
+        String[] parts = mensaje.split(";");
+        String[][] datosServer = new String[parts.length][];
+        for (int i = 0; i < parts.length; i++) {
+            datosServer[i] = parts[i].split(":");
+        }
+        // put the String[][] to the table
+        ObservableList<ObservableList<String>> data = tblArchivos.getItems();
+        for (String[] strings : datosServer) {
+            ObservableList<String> row = FXCollections.observableArrayList();
+            row.addAll(Arrays.asList(strings));
+            data.add(row);
+        }
+        tblArchivos.setItems(data);
+
+
     }
 }
---
+++ src/main/java/com/example/turtletorrent/ServidorTorrent.java
@@ -0,0 +1,5 @@
+package com.example.turtletorrent;
+
+public class ServidorTorrent {
+
+}
